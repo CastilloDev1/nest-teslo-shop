150956b2154dbf9fa19fb47f81a204347aef91ac
Fabian Castillo <fabianboc960@hotmail.com>
2025-02-03 22:51:26 -0500

    feat(crud): :sparkles: CRUD products: Get and Delete
    Creación completa del CRUD del modulo Products: Get y Delete.

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,33 @@
+import { IsArray, IsIn, IsInt, IsNumber, IsOptional, IsPositive, IsString, MinLength } from "class-validator";
+
+export class CreateProductDto {
+
+    @IsString()
+    @MinLength(1) //Debe ser mayor o igual a 1 character
+    title: string;
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?: number;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsString()
+    @IsOptional()
+    slug?: string;
+
+    @IsInt()      //Debe ser numerico con decimales
+    @IsPositive()
+    @IsOptional()
+    stock?: number;
+
+    @IsString({ each: true }) //Debe tener las mismas reglas para los elementos del array
+    @IsArray()
+    sizes: string[];
+
+    @IsIn(['men','women','kid','unisex']) //Debe permitir solo los valores asignados en @IsIn(...)
+    gender: string;
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,56 @@
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ProductsService {
+
+  private readonly logger = new Logger('ProductsService');
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>
+  ){}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+      return product;
+    } catch (error) {
+      this.handlerDBException(error);
+    }
+  }
+
+  async findAll() {
+    try {
+      return await this.productRepository.find();
+    } catch (error) {
+      
+    }
+  }
+
+  async findOne(id: string) {
+    const product = await this.productRepository.findOneBy({id});
+    if (!product) throw new NotFoundException(`Product with id ${id} not found`);
+    return product;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  async remove(id: string) {
+    const product = await this.findOne(id);
+    await this.productRepository.remove(product);
+  }
+
+  private handlerDBException(error: any): void {
+    if( error.code === '23505' ) throw new BadRequestException(error.detail);
+    this.logger.error(error);
+    throw new InternalServerErrorException(error.details);
+  }
+}
